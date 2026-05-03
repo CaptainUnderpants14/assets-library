7b6f44d0fe523529f7bac3a12cab1321ecdc1a65
CaptainUnderpants14 <parassharma8890@gmail.com>
2026-05-03 19:32:42 +0530

    update build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/index.js",
+      name: "AssetsLibrary",
+      formats: ["es", "cjs"], // 🔥 important
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
+});
